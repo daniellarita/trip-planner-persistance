0459de81720af3c7ead281b2db86183a3267cb19
Kaushik Nagaraj <kaushikstan@gmail.com>
2016-11-12 11:37:35 -0500

    options completed

--- public/js/options.js
+++ public/js/options.js
@@ -2,7 +2,8 @@
 //
 // Setup event handler for adding an attraction to the current
 // day's itinerary.
-$(function initializeOptions() {
+
+function initializeOptions() {
   var hotelSelector = $('#hotel-choices');
   var restaurantSelector = $('#restaurant-choices');
   var activitySelector = $('#activity-choices');
@@ -25,5 +26,44 @@ $(function initializeOptions() {
     let attraction = Attractions.find(type, id);
     Trip.addToCurrent(attraction);
   });
-});
+}
 
+var restaurants, hotels, activities;
+
+$(function() {
+  $.ajax({
+    method: 'GET',
+    url: '/restaurants'
+  })
+  .then(function(responseData) {
+    restaurants = responseData;
+    console.log(restaurants);
+  })
+  .then(function() {
+    return $.ajax({
+      method: 'GET',
+      url: '/hotels'
+    });
+  })
+  .then(function(responseData) {
+    hotels = responseData;
+    console.log(hotels);
+  })
+  .then(function() {
+    return $.ajax({
+      method: 'GET',
+      url: '/activities'
+    });
+  })
+  .then(function(responseData) {
+    activities = responseData;
+    console.log(activities);
+  })
+  .then(initializeOptions)
+  .then(function() {
+    $(Trip.load);
+  })
+  .then(null, function (errorObj) {
+    console.log(errorObj);
+  });
+});
